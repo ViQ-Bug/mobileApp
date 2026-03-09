258f0607dfde4b2686d6c283e51f8e1b148f37fb
ViQ-Bug <ndc079.code.it@gmail.com>
2026-03-09 13:07:52 +0700

    startServer

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,9 @@ if(ENV.NODE_ENV === "production") {
     })
 }
 
-app.listen(ENV.PORT,() =>{
-    console.log("server is running");
-    connectDB();
-} );
+const startServer = async ()=>{
+    await connectDB();
+    app.listen(ENV.PORT,()=>console.log(`Server is running on port ${ENV.PORT}`));
+}
+
+startServer();

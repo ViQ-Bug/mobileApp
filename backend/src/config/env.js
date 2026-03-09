b019def843ecff32fba82adf5637388868cbfe43
ViQ-Bug <ndc079.code.it@gmail.com>
2026-03-09 16:57:28 +0700

    5h9/3

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL,
-    CLECK_PUBLISHABLE_KEY: process.env.CLECK_PUBLISHABLE_KEY,
-    CLECK_SECRET_KEY: process.env.CLECK_SECRET_KEY,
-    INNGESSION_KEY: process.env.INNGESSION_KEY,
-    COULDINARY_API_KEY: process.env.COULDINARY_API_KEY,
-    COULDINARY_API_SECRET: process.env.COULDINARY_API_SECRET,
-    COULDINARY_CLOUD_NAME: process.env.COULDINARY_CLOUD_NAME
-}
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  CLECK_PUBLISHABLE_KEY: process.env.CLECK_PUBLISHABLE_KEY,
+  CLECK_SECRET_KEY: process.env.CLECK_SECRET_KEY,
+  INNGESSION_KEY: process.env.INNGESSION_KEY,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+};
